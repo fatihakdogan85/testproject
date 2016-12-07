2258ba75c70a61c3ade30bca651bffae77a39422
fatihakdogan85 <fatih.akdgn@gmail.com>
2016-12-07 10:09:42 +0300

    My first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, q = 3;
+
+            x = ++q + 4;
+            x = q++ + 4;
+
+
+            string textTest = "12 bir sayıdır" + 35;
+
+            int numberTest = 12;
+
+            string convertTest = numberTest.ToString();
+
+
+
+        }
+    }
+}
